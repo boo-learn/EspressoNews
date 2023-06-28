4485c8167cbe38c5d9e6a6e04bf08a305747a534
hobrus <bhobrus@gmail.com>
2023-06-29 02:22:31 +0300

    another fix

--- shared/celery_app.py
+++ shared/celery_app.py
@@ -1,15 +1,12 @@
 from celery import Celery
 
 from shared.celery_beat_schedule import beat_schedule
-from subscription_service.tasks import subscribe_task, unsubscribe_task
-
 
 def create_celery_app(name, broker, task_routes, timezone='UTC'):
     app = Celery(name, broker=broker)
     app.conf.task_routes = task_routes
     app.conf.timezone = timezone
     return app
-
 
 subscriptions_task_routes = {
     'subscription_service.tasks.subscribe_task': {'queue': 'subscriptions', 'rate_limit': '1/m'},
@@ -29,4 +26,3 @@ news_collector_celery_app = create_celery_app('news_collector', 'redis://redis:6
                                               news_collector_task_routes)
 summarize_celery_app = create_celery_app('summary_service', 'redis://redis:6379/0', summarize_task_routes)
 news_collector_celery_app.conf.beat_schedule = beat_schedule
-
