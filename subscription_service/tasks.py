4b74fda18956233b49606f867036d97990ea9ed3
hobrus <bhobrus@gmail.com>
2023-06-30 04:14:16 +0300

    update logging

--- subscription_service/tasks.py
+++ subscription_service/tasks.py
@@ -15,24 +15,33 @@ logger = logging.getLogger(__name__)
 
 
 async def connect_and_subscribe(loaded_account, channel_username):
-    loaded_client = TelegramClient(StringSession(loaded_account.session_string), loaded_account.api_id,
-                                   loaded_account.api_hash)
-    logger.info('Get telegram client')
-    await loaded_client.connect()
-    await subscribe_to_channel(loaded_client, channel_username)
-    logger.info('subscribe start ok!')
-    await loaded_client.disconnect()
+    try:
+        loaded_client = TelegramClient(StringSession(loaded_account.session_string), loaded_account.api_id,
+                                    loaded_account.api_hash)
+        logger.info('Created telegram client for account: %s', loaded_account.username)
+        await loaded_client.connect()
+        logger.info('Connected to telegram for account: %s', loaded_account.username)
+        await subscribe_to_channel(loaded_client, channel_username)
+        logger.info('Subscribed account: %s to channel: %s', loaded_account.username, channel_username)
+        await loaded_client.disconnect()
+        logger.info('Disconnected from telegram for account: %s', loaded_account.username)
+    except Exception as e:
+        logger.error('Error in connect_and_subscribe for account: %s, Error: %s', loaded_account.username, str(e))
 
 @celery_app.task(name='tasks.subscribe_task')
 def subscribe_task(account_id, channel_username):
-    logger.info(f'Starting subscription task for account {account_id} and channel {channel_username}')
+    logger.info('Starting subscription task for account: %s and channel: %s', account_id, channel_username)
     loaded_account = get_account_from_db(account_id)
     if loaded_account:
-        logger.info('Have logged account')
+        logger.info('Loaded account: %s from database', loaded_account.username)
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        loop.run_until_complete(connect_and_subscribe(loaded_account, channel_username))
-        loop.close()
-        logger.info(f'Successfully subscribed account {account_id} to channel {channel_username}')
+        try:
+            loop.run_until_complete(connect_and_subscribe(loaded_account, channel_username))
+            logger.info('Successfully subscribed account: %s to channel: %s', account_id, channel_username)
+        except Exception as e:
+            logger.error('Error in subscribe_task for account: %s, Error: %s', account_id, str(e))
+        finally:
+            loop.close()
     else:
-        logger.error(f'Account {account_id} not found')
+        logger.error('Account not found in database: %s', account_id)
