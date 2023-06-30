0eb0af7a5363ca5ab3d939b42bcd36fa39fa8cee
hobrus <bhobrus@gmail.com>
2023-06-30 04:05:14 +0300

    test2

--- subscription_service/tasks.py
+++ subscription_service/tasks.py
@@ -14,26 +14,25 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+async def connect_and_subscribe(loaded_account, channel_username):
+    loaded_client = TelegramClient(StringSession(loaded_account.session_string), loaded_account.api_id,
+                                   loaded_account.api_hash)
+    logger.info('Get telegram client')
+    await loaded_client.connect()
+    await subscribe_to_channel(loaded_client, channel_username)
+    logger.info('subscribe start ok!')
+    await loaded_client.disconnect()
+
 @celery_app.task(name='tasks.subscribe_task')
 def subscribe_task(account_id, channel_username):
     logger.info(f'Starting subscription task for account {account_id} and channel {channel_username}')
     loaded_account = get_account_from_db(account_id)
     if loaded_account:
-        logger.info('Have logged acount')
-        with ThreadPoolExecutor(max_workers=1) as executor:
-            future = executor.submit(connect_and_subscribe, loaded_account, channel_username)
-            future.result()
+        logger.info('Have logged account')
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(connect_and_subscribe(loaded_account, channel_username))
+        loop.close()
         logger.info(f'Successfully subscribed account {account_id} to channel {channel_username}')
     else:
         logger.error(f'Account {account_id} not found')
-
-
-def connect_and_subscribe(loaded_account, channel_username):
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    loaded_client = TelegramClient(StringSession(loaded_account.session_string), loaded_account.api_id,
-                                   loaded_account.api_hash)
-    logger.info('Get telegram client')
-    loaded_client.connect()
-    subscribe_to_channel(loaded_client, channel_username)
-    logger.info('subscribe start ok!')
-    loaded_client.disconnect()
