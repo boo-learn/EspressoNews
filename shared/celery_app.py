ce7e7b84cf3ad7dea3d91d0c07b87f16e1fed8c9
Weorano <ignatov.roman.a@mail.ru>
2023-07-11 14:48:38 +0300

    add property reconnect to broker

--- shared/celery_app.py
+++ shared/celery_app.py
@@ -6,6 +6,10 @@ backend_url = 'redis://redis:6379/0'
 
 celery_app = Celery('tasks', broker=broker_url, backend=backend_url)
 
+celery_app.conf.update({
+    'broker_connection_retry_on_startup': True
+})
+
 celery_app.conf.beat_schedule = {
     'hourly-task-collect-news': {
         'task': 'tasks.collect_news',
@@ -14,7 +18,7 @@ celery_app.conf.beat_schedule = {
     },
     'daily-task-get-digest': {
         'task': 'tasks.generate_all_digests_for_users',
-        'schedule': 120,  # crontab(hour=6, minute=50),
+        'schedule': 600,  # crontab(hour=6, minute=50),
         'args': (),
     },
 }
