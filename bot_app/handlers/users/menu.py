34f51c7334cf529d7c69a62f0a929d2d30d25cc4
Weorano <ignatov.roman.a@mail.ru>
2023-07-15 23:00:20 +0300

    add donate button

--- bot_app/handlers/users/menu.py
+++ bot_app/handlers/users/menu.py
@@ -4,15 +4,15 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 
-from bot_app.data.messages import gen_menu_mess
+from bot_app.data.messages import gen_menu_mess, gen_thank_you_mess
 from bot_app.keyboards.default import kb_menu, get_kb_settings
-from bot_app.keyboards.inlines import ikb_help
+from bot_app.keyboards.inlines import ikb_help, get_donate_button
 from bot_app.loader import dp
 from bot_app.logic import ChannelLogicHandler
 
 
 @dp.message_handler(Command('menu'))
-async def cmd_menu(message: types.Message, state: FSMContext):
+async def cmd_menu(message: types.Message):
     await message.delete()
     await message.answer(gen_menu_mess(), reply_markup=kb_menu)
 
@@ -30,8 +30,7 @@ async def menu_button_my_channels(message: types.Message):
 
 @dp.message_handler(regexp=re.compile(r'^Донат$', re.IGNORECASE))
 async def menu_button_my_channels(message: types.Message):
-    await message.answer('Перевод на карту:')
-    await message.answer('<b>1111 2222 3333 4444</b>')
+    await message.answer(gen_thank_you_mess(), reply_markup=get_donate_button())
 
 
 @dp.message_handler(regexp=re.compile(r'^Помощь$', re.IGNORECASE))
--- bot_app/keyboards/inlines/__init__.py
+++ bot_app/keyboards/inlines/__init__.py
@@ -2,3 +2,4 @@ from .start import ikb_start, ikb_after_start
 from .back import ikb_back
 from .channels import get_choose_channels_ikb, get_sure_unsubscribe_ikb, get_sure_subscribe_ikb, get_posts_rubrics
 from .help import ikb_help
+from .donate import get_donate_button
