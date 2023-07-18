bc14c2b7d2e2cbe21db96fad6b49fb014a7d4370
Weorano <ignatov.roman.a@mail.ru>
2023-07-18 21:34:36 +0300

    change logic on multi turbo

--- summary_service/db_utils.py
+++ summary_service/db_utils.py
@@ -1,7 +1,8 @@
 from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session, joinedload
 
-from shared.models import Post, GPTAccount
+from shared.models import Post, GPTAccount, Role, Intonation, Summary
 from shared.database import async_session
 
 from sqlalchemy import select
@@ -21,14 +22,11 @@ async def get_posts_without_summary_async(limit: int = 50):
         return result.unique().scalars().all()
 
 
-async def update_post_summary_async(post_id: int, summary: str):
+async def update_post_summary_async(post_id: int, summary: str, role_id: str, intonation_id: int):
     async with async_session() as db:
-        result = await db.execute(
-            select(Post).where(Post.post_id == post_id)
-        )
-        post = result.scalar_one()
-        post.summary = summary
-        await db.commit()
+        new_summary = Summary(content=summary, role_id=role_id, intonation_id=intonation_id, post_id=post_id)
+        db.add(new_summary)
+        return await db.commit()
 
 
 async def get_active_gpt_accounts_async():
@@ -45,3 +43,23 @@ async def update_chatgpt_account_async(api_key: str):
         acc = result.scalar_one()
         acc.is_active = False
         await db.commit()
+
+
+async def get_role_settings_options_for_gpt():
+    try:
+        async with async_session() as session:
+            stmt = select(Role)
+            result = await session.execute(stmt)
+            return [row for row in result.scalars().all()]
+    except SQLAlchemyError as e:
+        raise e
+
+
+async def get_intonation_settings_options_for_gpt():
+    try:
+        async with async_session() as session:
+            stmt = select(Intonation)
+            result = await session.execute(stmt)
+            return [row for row in result.scalars().all()]
+    except SQLAlchemyError as e:
+        raise e
--- summary_service/summary_main.py
+++ summary_service/summary_main.py
@@ -1,24 +1,26 @@
 import asyncio
 import logging
 import re
-
+import itertools
 from shared.config import RABBIT_HOST
-from shared.models import Post
+from shared.models import Post, Role, Intonation
 from shared.rabbitmq import Subscriber, QueuesType
 from summary_service.chat_gpt import ChatGPT
 from db_utils import get_posts_without_summary_async, update_post_summary_async, get_active_gpt_accounts_async, \
-    update_chatgpt_account_async
+    update_chatgpt_account_async, get_role_settings_options_for_gpt, get_intonation_settings_options_for_gpt
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-async def generate_summary(chatgpt: ChatGPT, post: Post):
+async def generate_summary(chatgpt: ChatGPT, post: Post, role_obj: Role, intonation_obj: Intonation):
     try:
         truncated_content = post.content[:10000]  # Truncate the content to 10,000 characters
 
-        messages = [{"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": f"Сделай этот текст максимально кратким и понятным: {truncated_content}"}]
+        messages = [
+            {"role": "system", "content": f"You are {role_obj.role} and use intonation {intonation_obj.intonation}"},
+            {"role": "user",
+             "content": f"Сделай этот текст максимально кратким и понятным: {truncated_content}"}]
 
         response = await chatgpt.generate_response(messages=messages, user_id=post.post_id, model="gpt-3.5-turbo-16k")
         summary = response['choices'][0]['message']['content']
@@ -26,7 +28,7 @@ async def generate_summary(chatgpt: ChatGPT, post: Post):
 
         logger.info(f"Summary {summary}")
 
-        return summary
+        return summary, role.id, intonation.id
     except Exception as e:
         error_message = str(e)
         if "Error: 429" in error_message:
@@ -44,6 +46,8 @@ async def generate_summary(chatgpt: ChatGPT, post: Post):
 async def generate_summaries():
     while True:
         posts = await get_posts_without_summary_async()
+        roles_objects_list = await get_role_settings_options_for_gpt()
+        intonation_objects_list = await get_intonation_settings_options_for_gpt()
 
         if not posts:
             break
@@ -58,15 +62,17 @@ async def generate_summaries():
 
             chatgpt_account = chatgpt_accounts.pop(0)
             chatgpt_accounts.append(chatgpt_account)
-
             gpt_instance = ChatGPT(chatgpt_account.api_key)
 
-            tasks.append(generate_summary(gpt_instance, post))
+            tasks.extend(
+                generate_summary(gpt_instance, post, role_obj, intonation_obj)
+                for role_obj, intonation_obj in itertools.product(roles_objects_list, intonation_objects_list)
+            )
 
-        summaries = await asyncio.gather(*tasks)
+        results = await asyncio.gather(*tasks)
 
-        for post, summary in zip(posts, summaries):
-            await update_post_summary_async(post.post_id, summary)
+        for post, (summary, role_id, intonation_id) in zip(posts, results):
+            await update_post_summary_async(post.post_id, summary, role_id, intonation_id)
 
 
 async def main():
@@ -74,6 +80,7 @@ async def main():
     subscriber = Subscriber(host=RABBIT_HOST, queue=QueuesType.summary_service)
     subscriber.subscribe(message_type="summarize_news", callback=generate_summaries)
     await subscriber.run()
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
