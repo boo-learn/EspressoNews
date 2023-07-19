10a4346d57e47efebb66696a4870db559a6b1492
Hobrus <bhobrus@gmail.com>
2023-07-19 12:06:35 +0300

    small fix

--- bot_app/databases/cruds/digest_crud.py
+++ bot_app/databases/cruds/digest_crud.py
@@ -32,11 +32,14 @@ class DigestCRUD:
         user_intonation = user_settings.intonation_id
 
         summaries = []
+        total_count = 0
         for post in digest.posts[offset:offset + limit]:
+            post_has_summary = False
             for summary in post.summaries:
                 if summary.role_id == user_role and summary.intonation_id == user_intonation:
-                    if summary.content:
+                    if summary.content is not None:
                         summaries.append(summary.content)
-
-        total_count = len(digest.posts)
+                        post_has_summary = True
+            if post_has_summary:
+                total_count += 1
         return summaries, total_count
