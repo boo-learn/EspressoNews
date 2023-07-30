9d23cee298299ca294fd249c0861c43c852e2e9a
Weorano <ignatov.roman.a@mail.ru>
2023-07-30 22:09:51 +0300

    add half function ban account

--- healthcheck_for_collect_news/db_utils.py
+++ healthcheck_for_collect_news/db_utils.py
@@ -1,9 +1,11 @@
-from sqlalchemy import select
+from random import choice
+
+from sqlalchemy import select, func, delete
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import selectinload
 
 from shared.database import async_session
-from shared.models import TelegramAccount
+from shared.models import TelegramAccount, Channel
 
 
 async def get_telethon_accounts_list():
@@ -14,5 +16,58 @@ async def get_telethon_accounts_list():
             accounts = result.scalars().all()
 
             return accounts or None
+    except SQLAlchemyError as e:
+        raise e
+
+
+async def get_random_account_exclude_most_subscribed():
+    async with async_session() as db:
+        # Получаем общее количество аккаунтов
+        total_accounts_stmt = select(func.count(TelegramAccount.account_id))
+        total_accounts = await db.scalar(total_accounts_stmt)
+
+        if total_accounts == 1:
+            total_accounts = 2  # Если аккаунт всего один остался, что бы он воевал до последнего
+
+        # Получаем список аккаунтов, исключая аккаунт с наибольшим числом подписок
+        accounts_stmt = (
+            select(TelegramAccount)
+            .outerjoin(Channel, TelegramAccount.account_id == Channel.account_id)
+            .group_by(TelegramAccount.account_id)
+            .order_by(func.count(Channel.channel_id).asc())
+            .limit(total_accounts - 1)
+        )
+        result = await db.execute(accounts_stmt)
+        accounts = result.scalars().all()
+
+        # Если нет аккаунтов для выбора, возвращаем None
+        if not accounts:
+            return None
+
+        return choice(accounts)
+
+
+async def delete_banned_account_and_reconnect_channels(banned_account_id, new_account, unlinked_channels):
+    try:
+        async with async_session() as db:
+            # Удаление забаненного аккаунта
+            await db.execute(
+                delete(TelegramAccount)
+                .where(TelegramAccount.account_id == banned_account_id)
+            )
+
+            # Переподключение каналов к новому аккаунту
+            for channel_name in unlinked_channels:
+                stmt = select(Channel).where(Channel.channel_name == channel_name)
+                result = await db.execute(stmt)
+                channel = result.scalars().first()
+
+                if channel is not None:
+                    channel.account_id = new_account.account_id
+                    await db.merge(channel)
+
+            # Закрепляем транзакцию
+            await db.commit()
+
     except SQLAlchemyError as e:
         raise e
--- healthcheck_for_collect_news/hc_collect_news_main.py
+++ healthcheck_for_collect_news/hc_collect_news_main.py
@@ -3,7 +3,8 @@ import logging
 from telethon import TelegramClient
 from telethon.errors import UserBannedInChannelError
 
-from healthcheck_for_collect_news.db_utils import get_telethon_accounts_list
+from healthcheck_for_collect_news.db_utils import get_telethon_accounts_list, \
+    get_random_account_exclude_most_subscribed, delete_banned_account_and_reconnect_channels
 
 logger = logging.getLogger(__name__)
 
@@ -16,12 +17,23 @@ async def main():
         return 'На данный момент не задействовано ни одного аккаунта.'
 
     account_subscriptions = {}
+
+    for account in enumerate(telethon_accounts):
+        account_subscriptions[account.account_id] = {
+            "account": {
+                "api_id": account.api_id,
+                "api_hash": account.api_hash,
+                "session_string": account.session_string
+            },
+            "channels": [channel.name for channel in account.channels]
+        }
+
     current_account_id = None
 
     for i, account_info in account_subscriptions.items():
         try:
             account = account_info["account"]
-            current_account_id = account["account_id"]
+            current_account_id = i
 
             db_channels = account_info["channels"]
             client = TelegramClient(account["session_string"], account["api_id"], account["api_hash"])
@@ -39,3 +51,15 @@ async def main():
         except UserBannedInChannelError:
             # Берём все каналы из бд и подписываемся на них другим аккаунтом Telethon
             logger.error(f"Аккаунт {current_account_id} забанен в канале.")
+            more_empty_account = await get_random_account_exclude_most_subscribed()
+            unlinked_subscriptions = account_subscriptions[current_account_id]['channels']
+            await delete_banned_account_and_reconnect_channels(
+                current_account_id,
+                more_empty_account,
+                unlinked_subscriptions
+            )
+
+            # подписаться самим аккаунтом telethon
+
+            await main()
+            return True
