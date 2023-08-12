b4e51128a58bc7ac29a1f3df52640668d1d21daf
Hobrus <bhobrus@gmail.com>
2023-08-13 01:53:33 +0300

    hotfix for pool

--- new_digest_microservice/digest_main.py
+++ new_digest_microservice/digest_main.py
@@ -8,11 +8,7 @@ import logging
 from shared.models import Post
 
 
-def fetch_and_clear_posts_atomic():
-    # Создание пула соединений
-    pool = redis.ConnectionPool(host='redis', port=6379, db=0)
-    r = redis.Redis(connection_pool=pool)
-
+def fetch_and_clear_posts_atomic(r):
     # Использование пайплайна для атомарных операций
     with r.pipeline() as pipe:
         pipe.multi()
@@ -26,9 +22,9 @@ def fetch_and_clear_posts_atomic():
     return [Post(**post) for post in posts]
 
 
-def run():
+def run(r):
     logging.info(f"Проверяем посты")
-    posts = fetch_and_clear_posts_atomic()
+    posts = fetch_and_clear_posts_atomic(r)
 
     # Вывести полученные посты в лог
     for post in posts:
@@ -36,13 +32,17 @@ def run():
 
 
 def main():
+    # Создание пула соединений
+    pool = redis.ConnectionPool(host='redis', port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     while datetime.datetime.now().second != 0:
         time.sleep(1)
 
     # Настроим расписание для запуска функции каждую минуту
-    schedule.every().minute.do(run)
+    schedule.every().minute.do(run, r)
 
     while True:
         schedule.run_pending()
